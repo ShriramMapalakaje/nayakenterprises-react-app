ac9329287b96135629217324264d6816fca99207
Shriram Mapalakaje <shriramnayak.m@gmail.com>
2025-09-20 11:51:24 +0530

    Update frontend with latest changes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -37,7 +37,7 @@ const MainPage = ({ darkMode, setDarkMode }) => {
     </div>
     <div className="intro-visual">
       <img 
-                    src={process.env.PUBLIC_URL + '/images/fileimage2.png'} 
+                    src={process.env.PUBLIC_URL + '/images/NayakLogo.png'} 
                     alt="Nayak Enterprises Logo" 
                      
                   />
